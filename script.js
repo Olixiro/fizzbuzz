67ef3f1dd003bb5ac4ed894b1cc2ebe694090e8a
Olixiro <245492173+Olixiro@users.noreply.github.com>
2025-12-30 13:31:49 -0700

    wrote out steps for project

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+Understand the problem : 
+"Write a program that takes a user’s input and prints the numbers from one to the number the user entered. However, for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz"
+
+1. set up a program to grab user input
+2. function to count from 1 to the number the user entered
+3. for every multiple of 3 print "Fizz"
+4. for every multiple of 5 print "Buzz"
+
+*/
